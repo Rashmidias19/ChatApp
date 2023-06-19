ea0945571e8a3c7f67f31bb71e4b17632b93e893
Rashmidias19 <rashmidias819@gmail.com>
2023-06-19 12:07:07 +0530

    ChatForm Created

---
+++ Work/src/main/java/ChatFormController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ChatFormController {
+    public AnchorPane dashboardPane;
+    public TextField txtMessage;
+    public Button btnCamera;
+    public TextArea txtArea;
+    public Button btnSend;
+
+    public void btnCameraOnAction(ActionEvent event) {
+    }
+
+    public void sendBtnOnAction(ActionEvent event) {
+    }
+}
--- Work/src/main/java/LoginFormController.java
+++ Work/src/main/java/LoginFormController.java
@@ -1,13 +1,23 @@
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXButton btnLogin;
     public TextField txtUserName;
     public AnchorPane dashboardPane;
 
-    public void loginBtnOnAction(ActionEvent event) {
+    public void loginBtnOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ChatFormForm.fxml"))));
+        stage.setTitle("VETCLOUD");
+        stage.centerOnScreen();
+        stage.show();
     }
 }
