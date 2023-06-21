c12e19769c2d6182769009969f82b11ff9938cd8
Rashmidias19 <rashmidias819@gmail.com>
2023-06-21 19:16:32 +0530

    emoji pane added and other changes

--- Work/src/main/java/ChatFormController.java
+++ Work/src/main/java/ChatFormController.java
@@ -94,42 +94,8 @@ public class ChatFormController extends Thread implements Initializable {
 
                     ImageView imageView = new ImageView(image);
 
-                    imageView.setFitWidth(70);
-                    imageView.setFitHeight(70);
-
-                    HBox hBox = new HBox(10);
-                    hBox.setAlignment(Pos.BOTTOM_RIGHT);//bottom-right
-                    //vBox.setAlignment(Pos.BOTTOM_LEFT);
-                    //hBox.setAlignment(Pos.CENTER_LEFT);
-
-                 ///   hBox.getChildren().add(imageView);
-                //for sending the images
-                    if (!cmd.equalsIgnoreCase(lblName.getText())) {
-                        vBox.setAlignment(Pos.BOTTOM_LEFT);//top left
-                        hBox.setAlignment(Pos.CENTER_LEFT);//center right
-
-                       Text text1 = new Text("  " + cmd + " :");
-                        hBox.getChildren().add(text1);
-                        hBox.getChildren().add(imageView);
-                    } else {
-                        hBox.setAlignment(Pos.BOTTOM_RIGHT);//bottom right
-                        hBox.getChildren().add(imageView);
-                        Text text1 = new Text(": Me ");
-                        hBox.getChildren().add(text1);
-                    }
-
-                    Platform.runLater(() -> vBox.getChildren().addAll(hBox));
-
-                }else if(fChar.equalsIgnoreCase("emj")){
-                    string = string.substring(3, string.length() - 1);
-
-                    File file = new File(string);
-                    Image image = new Image(file.toURI().toString());
-
-                    ImageView imageView = new ImageView(image);
-
-                    imageView.setFitWidth(70);
-                    imageView.setFitHeight(70);
+                    imageView.setFitWidth(150);
+                    imageView.setFitHeight(150);
 
                     HBox hBox = new HBox(10);
                     hBox.setAlignment(Pos.BOTTOM_RIGHT);//bottom-right
@@ -141,23 +107,24 @@ public class ChatFormController extends Thread implements Initializable {
                     if (!cmd.equalsIgnoreCase(lblName.getText())) {
                         vBox.setAlignment(Pos.BOTTOM_LEFT);//top left
                         hBox.setAlignment(Pos.CENTER_LEFT);//center right
-
+                        //to set the image to other clients
                         Text text1 = new Text("  " + cmd + " :");
                         hBox.getChildren().add(text1);
                         hBox.getChildren().add(imageView);
                     } else {
                         hBox.setAlignment(Pos.BOTTOM_RIGHT);//bottom right
                         hBox.getChildren().add(imageView);
-                        Text text1 = new Text("");
+                        Text text1 = new Text(": Me ");
                         hBox.getChildren().add(text1);
                     }
 
                     Platform.runLater(() -> vBox.getChildren().addAll(hBox));
+
                 }
                 else {
                     TextFlow tempTextFlow = new TextFlow();
 
-                    if (!cmd.equalsIgnoreCase(/*LoginFormController.name*/lblName.getText() + ":")) {
+                    if (!cmd.equalsIgnoreCase(/*LoginFormController.name*/lblName.getText()+ ":")) {
                         Text name = new Text(cmd + " ");
                         name.getStyleClass().add("name");
                         tempTextFlow.getChildren().add(name);
@@ -177,12 +144,12 @@ public class ChatFormController extends Thread implements Initializable {
                         hBox.setAlignment(Pos.CENTER_LEFT);
                         hBox.getChildren().add(textFlow);
                     } else {
-                        Text text1 = new Text(fullMsg + ": Me");
-                        TextFlow textFlow1 = new TextFlow(text1);
-                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
-                        hBox.getChildren().add(textFlow1);
-                        textFlow1.setStyle("-fx-background-color:#7bed9f;" + "-fx-background-radius: 20px;" + "-fx-font-size: 17px;");
-                        textFlow1.setPadding(new Insets(5, 10, 5, 10));
+//                        Text text1 = new Text(fullMsg + ": Me");
+//                        TextFlow textFlow1 = new TextFlow(text1);
+//                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+//                        hBox.getChildren().add(textFlow1);
+//                        textFlow1.setStyle("-fx-background-color:#7bed9f;" + "-fx-background-radius: 20px;" + "-fx-font-size: 17px;");
+//                        textFlow1.setPadding(new Insets(5, 10, 5, 10));
                     }
                     Platform.runLater(() -> vBox.getChildren().addAll(hBox));
                 }
@@ -249,103 +216,21 @@ public class ChatFormController extends Thread implements Initializable {
     }
 
     public void emojiBtnOnAction(MouseEvent mouseEvent) {
+        emojiPane.setVisible(true);
     }
 
-    ArrayList labels = new ArrayList();
-
-    public void setEmoji() {
-        String[] emojis = new String[]{
-                "\uD83D\uDE00", "\uD83D\uDE03", "\uD83D\uDE04", "\uD83D\uDE01", "\uD83D\uDE06", "\uD83E\uDD23",
-                "\uD83D\uDE02", "\uD83D\uDE42", "\uD83D\uDE43", "\uD83D\uDE09", "\uD83D\uDE0A", "\uD83D\uDE31",
-                "\uD83D\uDE07", "\uD83E\uDD70", "\uD83D\uDE0D", "\uD83E\uDD29", "\uD83D\uDE18", "\uD83D\uDE17",
-                "\uD83D\uDE0F", "\uD83D\uDE12", "\uD83D\uDE1E", "\uD83D\uDE14", "\uD83D\uDE1F", "\uD83D\uDE15",
-                "\uD83D\uDE41", "\uD83D\uDE23", "\uD83D\uDE16", "\uD83D\uDE2B", "\uD83D\uDE29", "\uD83D\uDE22",
-                "\uD83D\uDE2D", "\uD83D\uDE2E", "\uD83D\uDE2F", "\uD83D\uDE32", "\uD83D\uDE33", "\uD83D\uDE35",
-                "\uD83D\uDE28", "\uD83D\uDE30", "\uD83D\uDE25", "\uD83D\uDE34", "\uD83D\uDE37", "\uD83E\uDD12",
-                "\uD83E\uDD15", "\uD83E\uDD22", "\uD83E\uDD2E", "\uD83E\uDD27", "\uD83E\uDD75", "\uD83E\uDD76",
-                "\uD83E\uDD74", "\uD83E\uDD2F", "\uD83E\uDD1F", "\uD83D\uDE20", "\uD83D\uDE21", "\uD83E\uDD2C",
-                "\uD83D\uDE08", "\uD83D\uDC7F", "\uD83D\uDC80", "\uD83D\uDCA9", "\uD83E\uDD21", "\uD83D\uDC79",
-                "\uD83D\uDC7A"};
-        Label label;
-        String y;
-
-        for (int i = 0; i < emojis.length; i++) {
-            label = new Label();
-            y = emojis[i];
-            int fontSize = 28;
-            label.setStyle("-fx-font-size: " + fontSize + "px;");
-            label.setText(y);
-            Label finalLabel = label;
-            label.setOnMouseClicked(e -> {
-                vBox.setAlignment(Pos.BOTTOM_RIGHT); // Align VBox content to the right
-                Text text = new Text("Me : " + finalLabel.getText());
-                Label labelSender = new Label();
-                Label labelMsg = new Label();
-                labelMsg.setStyle("-fx-font-size: " + fontSize + "px;");
-                labelSender.setText("Me : ");
-                labelMsg.setText(finalLabel.getText());
-                text.setTextAlignment(TextAlignment.RIGHT); // Align text to the right
-                HBox hBox = new HBox(10);
-                hBox.setAlignment(Pos.BOTTOM_RIGHT);//bottom left
-                HBox hBox1 = new HBox();//extrs
-                hBox1.setAlignment(Pos.CENTER_RIGHT);//center right
-                hBox.getChildren().add(labelSender);
-                hBox.getChildren().add(labelMsg);
-                Platform.runLater(() -> vBox.getChildren().addAll(hBox));
-//                try {
-//                    dataOutputStream.writeUTF("emj " + finalLabel.getText());
-//                    dataOutputStream.flush();
-//                } catch (IOException ioException) {
-//                    ioException.printStackTrace();
-//                }
-                writer.println(lblName.getText() + " " + "emj");
-                writer.flush();
-            });
-            labels.add(label);
-            addEmojis();
-        }
-
-    }
-    public void addEmojis() {
-        HBox box1 = new HBox();
-        HBox box2 = new HBox();
-        HBox box3 = new HBox();
-        for (int j = 0; j <labels.size(); j++) {
-            if (j<=15){
-                box1.getChildren().add((Node) labels.get(j));
-            }else if (j<=30){
-                box2.getChildren().add((Node) labels.get(j));
-            }else if (j<=45){
-                box3.getChildren().add((Node) labels.get(j));
-            }
-
-        }
-//            emoji.setAlignment(Pos.TOP_LEFT);
-        emoji.getChildren().addAll(box1,box2,box3);
-
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         connectSocket();
-        setEmoji();
+        emojiPane.setVisible(false);
         lblName.setText(LoginFormController.name);
 
     }
 
-    public void sendEmoji(File filePath){
-        String msg = String.valueOf((filePath));
+    public void sendEmoji(String msg){
         writer.println(lblName.getText() + ": " + msg);
-        Image image = new Image(filePath.toURI().toString());
-
-        ImageView imageView = new ImageView(image);
-
-        imageView.setFitWidth(70);
-        imageView.setFitHeight(70);
-
-        HBox hBox1 = new HBox(10);
-        hBox1.setAlignment(Pos.BOTTOM_RIGHT);
-        HBox hBox = new HBox();
+        HBox hBox=new HBox();
         hBox.setAlignment(Pos.CENTER_RIGHT);
         hBox.setPadding(new Insets(5, 5, 5, 10));
         Text text = new Text("Me : "+msg);
@@ -368,4 +253,51 @@ public class ChatFormController extends Thread implements Initializable {
 //        txtAreaMsg.appendText("Me : "+"\uD83D\uDE42");
     }
 
+    public void emoji1OnAction(MouseEvent mouseEvent) {
+        String message=( "\uD83D\uDE02");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji2OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE01");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji3OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE0D");
+        emojiPane.setVisible(false);
+       sendEmoji(message);
+    }
+
+    public void emoji4OnAction(MouseEvent mouseEvent) {
+        String message=( "\uD83D\uDE2D");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji5OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE41");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji6OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE0A");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji7OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE35");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
+
+    public void emoji8OnAction(MouseEvent mouseEvent) {
+        String message=("\uD83D\uDE2B");
+        emojiPane.setVisible(false);
+        sendEmoji(message);
+    }
 }
